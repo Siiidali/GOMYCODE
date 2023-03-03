1da658266c117e48b3405cff220adbef3927f100
Si-saber Sidali <dadisisaber@gmail.com>
2023-03-03 12:02:09 +0100

    async js 2

--- JAVASCRIPT/async.js
+++ JAVASCRIPT/async.js
@@ -1,13 +1,47 @@
-const request = new XMLHttpRequest();
+// console.log(1);
+// fetch("./data/sidali.json")
+//   .then((res) => {
+//     console.log(res);
+//     const data = res.json();
+//     console.log(data);
+//     return data;
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-request.addEventListener("readystatechange", () => {
-  //   console.log(request, request.readyState);
-  if (request.readyState === 4) {
-    console.log(request);
-    console.log(request.responseText);
+// console.log(2);
+
+const getTodos = async () => {
+  try {
+    const res1 = await fetch("./data/sidali.json");
+    const data1 = await res1.json();
+    console.log(data1);
+    const res2 = await fetch("./data/mohamed.json");
+    const data2 = await res2.json();
+    console.log(data2);
+    // throw new Error("sidali");
+  } catch (error) {
+    console.log(error.message);
   }
-});
+};
+getTodos();
+//  example !
 
-request.open("GET", "https://jsonplaceholder.typicode.com/todos");
+// const getSomething = () => {
+//   return new Promise((resolve, reject) => {
+//     // fetch data
+//     reject("error");
+//   });
+// };
 
-request.send();
+// getSomething()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
